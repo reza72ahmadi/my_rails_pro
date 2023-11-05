81a7378dcff6564947db85d5e6032ffe2a018bd7
reza72ahmadi <reza72.ahmadi@gmail.com>
2023-11-05 22:46:41 +0430

    show added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
